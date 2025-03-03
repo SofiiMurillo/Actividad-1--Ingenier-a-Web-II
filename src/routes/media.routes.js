62b7e2ad4ee3d5b79bdeaf1c137203aafe73dde7
SofiiMurillo <sofimurillolopez7@gmail.com>
2025-03-03 17:29:52 -0500

    Caso de uso para crear un dato/media

--- src/routes/media.routes.js
+++ src/routes/media.routes.js
@@ -3,11 +3,19 @@ import { Router } from "express";
 import {
   obtenerMedia,
   obtenermediaPorId,
+  crearMedia,
+  eliminarMedia,
+  actualizarMedia,
 } from "../controllers/media.controllers.js";
 
 const router = Router();
 
 router.get("/media", obtenerMedia);
+
 router.get("/media/:id", obtenermediaPorId);
+
+router.post("/media", crearMedia);
+
+router.delete("/media/:id", eliminarMedia);
 
 export default router;
