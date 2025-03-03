2c6b26fa25853d8edf1bd7959a026060801a3dee
SofiiMurillo <sofimurillolopez7@gmail.com>
2025-03-03 12:17:47 -0500

    Caso d euno para obtener tipos por id

--- src/controllers/tipos_multimedia.controllers.js
+++ src/controllers/tipos_multimedia.controllers.js
@@ -4,3 +4,17 @@ export const obtenerTipos = async (req, res) => {
   const { rows } = await pool.query("SELECT * FROM tipos_multimedia");
   res.json(rows);
 };
+
+export const obtenerDatosPorId = async (req, res) => {
+  const { id } = req.params;
+
+  const { rows } = await pool.query(
+    "SELECT * FROM tipos_multimedia WHERE id = $1",
+    [id]
+  );
+
+  if (rows.length === 0) {
+    return res.status(404).json({ Message: "Tipo no existente" });
+  }
+  res.json(rows);
+};
--- src/routes/tipos_multimedia.routes.js
+++ src/routes/tipos_multimedia.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
-import { obtenerTipos } from "../controllers/tipos_multimedia.controllers.js";
+import {
+  obtenerTipos,
+  obtenerDatosPorId,
+} from "../controllers/tipos_multimedia.controllers.js";
 
 const router = Router();
 
 router.get("/tipos_multimedia", obtenerTipos);
+router.get("/tipos_multimedia/:id", obtenerDatosPorId);
 
 export default router;
